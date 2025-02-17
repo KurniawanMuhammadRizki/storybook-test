4549791a6442d5a3fca774ecd7c4d17334dcedb0
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2025-02-17 15:45:02 +0700

    feat: Add UserTable

---
+++ src/components/table/UserTable.tsx
@@ -0,0 +1,31 @@
+import { Table } from "@chakra-ui/react";
+import { UserData } from "./Types";
+
+interface UserTableProps {
+  data: UserData[];
+}
+
+export const UserTable: React.FC<UserTableProps> = ({ data }) => {
+  return (
+    <Table.Root size="sm">
+      <Table.Header>
+        <Table.Row>
+          <Table.ColumnHeader>Name</Table.ColumnHeader>
+          <Table.ColumnHeader>Email</Table.ColumnHeader>
+          <Table.ColumnHeader textAlign="end">Role</Table.ColumnHeader>
+          <Table.ColumnHeader>Action</Table.ColumnHeader>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {data.map((item, index) => (
+          <Table.Row key={index}>
+            <Table.Cell>{item.name}</Table.Cell>
+            <Table.Cell>{item.email}</Table.Cell>
+            <Table.Cell>{item.role}</Table.Cell>
+            <Table.Cell textAlign="end">action btn</Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  );
+};
