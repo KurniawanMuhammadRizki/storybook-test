858c063b886757bfab6f4b6197582d6a65025cb9
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2025-02-17 22:00:57 +0700

    test: fix test mockImplementation

--- src/components/button/ActionButton.test.tsx
+++ src/components/button/ActionButton.test.tsx
@@ -1,9 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { vi } from "vitest";
-import { ActionButton } from "./ActionButton";
+
 import { Provider } from "@/components/ui/provider";
 import userEvent from "@testing-library/user-event";
+import { ActionButton } from "./ActionButton";
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
 
 const renderWithChakra = (component: React.ReactElement) => {
   return render(<Provider>{component}</Provider>);
